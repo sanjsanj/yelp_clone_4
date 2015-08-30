f72b2ff5cbb87cc61252b13ef5141528c6eafa5b
sanjsanj <sanjsanj@hotmail.com>
2015-08-30 12:43:43 +0100

    restaurant can be added in the backend and update the view properly

--- spec/features/restaurant_feature_spec.rb
+++ spec/features/restaurant_feature_spec.rb
@@ -1,11 +1,32 @@
 require 'rails_helper'
 
-feature 'restaurants' do
-  context 'no restaurants have been added' do
-    scenario 'should display a prompt to add a restaurant' do
-      visit restaurants_path
+feature 'restaurants page' do
+  before do
+    visit restaurants_path
+  end
+
+  context 'when no restaurants exist' do
+    scenario 'informs user that there are no restaurants' do
       expect(page).to have_content 'No restaurants yet'
+    end
+
+    scenario 'displays a prompt to add a restaurant' do
       expect(page).to have_link 'Add a restaurant'
+    end
+  end
+
+  context 'when restaurants are added to the backend' do
+    before do
+      Restaurant.create name: 'KFC'
+      visit restaurants_path
+    end
+
+    scenario 'does not say that no restaurants exist' do
+      expect(page).not_to have_content 'No restaurants yet'
+    end
+
+    scenario 'displays restaurant name' do
+      expect(page).to have_content 'KFC'
     end
   end
 end
