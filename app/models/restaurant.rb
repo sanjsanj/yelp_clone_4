d98abd7f8f1d3ba5dddba5328955c4ca410b579e
sanjsanj <sanjsanj@hotmail.com>
2015-09-08 21:14:22 +0100

    review belongs to restaurant

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,6 @@
 class Restaurant < ActiveRecord::Base
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
+  def create_review review_params
+    self.reviews.create review_params
+  end
 end
